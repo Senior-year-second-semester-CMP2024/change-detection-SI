549e986114e9c29dfa4c65b2cc10de6755ecdbb6
markyasser <82395903+markyasser@users.noreply.github.com>
2024-05-12 16:14:35 +0300

    report

--- models/siam_nested_unet.py
+++ models/siam_nested_unet.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 import torch
 
-class conv_block_nested(nn.Module):
+class Conv_Block(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch):
-        super(conv_block_nested, self).__init__()
+        super(Conv_Block, self).__init__()
         self.activation = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(mid_ch)
@@ -23,13 +23,9 @@ class conv_block_nested(nn.Module):
 
 
 class up(nn.Module):
-    def __init__(self, in_ch, bilinear=False):
+    def __init__(self, in_ch):
         super(up, self).__init__()
-
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2,mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_ch, in_ch, 2, stride=2)
+        self.up = nn.ConvTranspose2d(in_ch, in_ch, 2, stride=2)
     def forward(self, x):
 
         x = self.up(x)
@@ -50,35 +46,35 @@ class Siam_NestedUNet_Conc(nn.Module):
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv0_0 = conv_block_nested(in_ch, filters[0], filters[0])
-        self.conv1_0 = conv_block_nested(filters[0], filters[1], filters[1])
+        self.conv0_0 = Conv_Block(in_ch, filters[0], filters[0])
+        self.conv1_0 = Conv_Block(filters[0], filters[1], filters[1])
         self.Up1_0 = up(filters[1])
-        self.conv2_0 = conv_block_nested(filters[1], filters[2], filters[2])
+        self.conv2_0 = Conv_Block(filters[1], filters[2], filters[2])
         self.Up2_0 = up(filters[2])
-        self.conv3_0 = conv_block_nested(filters[2], filters[3], filters[3])
+        self.conv3_0 = Conv_Block(filters[2], filters[3], filters[3])
         self.Up3_0 = up(filters[3])
-        self.conv4_0 = conv_block_nested(filters[3], filters[4], filters[4])
+        self.conv4_0 = Conv_Block(filters[3], filters[4], filters[4])
         self.Up4_0 = up(filters[4])
 
-        self.conv0_1 = conv_block_nested(filters[0] * 2 + filters[1], filters[0], filters[0])
-        self.conv1_1 = conv_block_nested(filters[1] * 2 + filters[2], filters[1], filters[1])
+        self.conv0_1 = Conv_Block(filters[0] * 2 + filters[1], filters[0], filters[0])
+        self.conv1_1 = Conv_Block(filters[1] * 2 + filters[2], filters[1], filters[1])
         self.Up1_1 = up(filters[1])
-        self.conv2_1 = conv_block_nested(filters[2] * 2 + filters[3], filters[2], filters[2])
+        self.conv2_1 = Conv_Block(filters[2] * 2 + filters[3], filters[2], filters[2])
         self.Up2_1 = up(filters[2])
-        self.conv3_1 = conv_block_nested(filters[3] * 2 + filters[4], filters[3], filters[3])
+        self.conv3_1 = Conv_Block(filters[3] * 2 + filters[4], filters[3], filters[3])
         self.Up3_1 = up(filters[3])
 
-        self.conv0_2 = conv_block_nested(filters[0] * 3 + filters[1], filters[0], filters[0])
-        self.conv1_2 = conv_block_nested(filters[1] * 3 + filters[2], filters[1], filters[1])
+        self.conv0_2 = Conv_Block(filters[0] * 3 + filters[1], filters[0], filters[0])
+        self.conv1_2 = Conv_Block(filters[1] * 3 + filters[2], filters[1], filters[1])
         self.Up1_2 = up(filters[1])
-        self.conv2_2 = conv_block_nested(filters[2] * 3 + filters[3], filters[2], filters[2])
+        self.conv2_2 = Conv_Block(filters[2] * 3 + filters[3], filters[2], filters[2])
         self.Up2_2 = up(filters[2])
 
-        self.conv0_3 = conv_block_nested(filters[0] * 4 + filters[1], filters[0], filters[0])
-        self.conv1_3 = conv_block_nested(filters[1] * 4 + filters[2], filters[1], filters[1])
+        self.conv0_3 = Conv_Block(filters[0] * 4 + filters[1], filters[0], filters[0])
+        self.conv1_3 = Conv_Block(filters[1] * 4 + filters[2], filters[1], filters[1])
         self.Up1_3 = up(filters[1])
 
-        self.conv0_4 = conv_block_nested(filters[0] * 5 + filters[1], filters[0], filters[0])
+        self.conv0_4 = Conv_Block(filters[0] * 5 + filters[1], filters[0], filters[0])
 
         self.final1 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
         self.final2 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
@@ -86,15 +82,21 @@ class Siam_NestedUNet_Conc(nn.Module):
         self.final4 = nn.Conv2d(filters[0], out_ch, kernel_size=1)
         self.conv_final = nn.Conv2d(out_ch * 4, out_ch, kernel_size=1)
 
+        # Initialize the weights of the model using best practices
         for m in self.modules():
+            # Check if the module is Conv2d
             if isinstance(m, nn.Conv2d):
+                # Initialize the weights of Conv2d using kaiming_normal method
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            # Check if the module is BatchNorm2d or GroupNorm
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                # Initialize the weights of BatchNorm2d to be 1, and bias to be 0
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
 
     def forward(self, xA, xB):
+        # X(i,0)
         '''xA'''
         x0_0A = self.conv0_0(xA)
         x1_0A = self.conv1_0(self.pool(x0_0A))
@@ -107,7 +109,7 @@ class Siam_NestedUNet_Conc(nn.Module):
         x2_0B = self.conv2_0(self.pool(x1_0B))
         x3_0B = self.conv3_0(self.pool(x2_0B))
         x4_0B = self.conv4_0(self.pool(x3_0B))
-
+        
         x0_1 = self.conv0_1(torch.cat([x0_0A, x0_0B, self.Up1_0(x1_0B)], 1))
         x1_1 = self.conv1_1(torch.cat([x1_0A, x1_0B, self.Up2_0(x2_0B)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0A, x0_0B, x0_1, self.Up1_1(x1_1)], 1))
